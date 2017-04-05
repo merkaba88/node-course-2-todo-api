3b9ed63197cab334216036b9f0a5e017aecdd7c8
Daniel Fegan <daniel.fegan1988@gmail.com>
2017-04-05 15:53:50 +0100

    Add PATCH /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const _ = require('lodash');
 const {ObjectID} = require('mongodb');
 
-const {mongoose} = require('./db/mongoose');
-const {Todo} = require('./models/todo');
-const {User} = require('./models/user');
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
 var app = express();
 const port = process.env.PORT || 3000;
@@ -12,6 +13,7 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
 // Routes
+
 // Create todo
 app.post('/todos', (req, res) => {
   var todo = new Todo({
@@ -69,6 +71,32 @@ app.delete('/todos/:id', (req, res) => {
     res.status(200).send({
       todo
     })
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
+// Update todo items
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']); //has subset of things the user passed to us as dont want user to be able to  update anything they choose e.g completedAt
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.status(200).send({todo});
   }).catch((e) => {
     res.status(400).send();
   });
